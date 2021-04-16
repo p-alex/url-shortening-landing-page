630ca505d9ddda8912ee5450fc683cd4e954041a
p-alex <alexey9021@gmail.com>
2021-04-16 21:31:35 +0300

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
 import MobileNavBar from "./components/MobileNavBar/MobileNavBar";
+import Header from "./components/Header/Header";
 import "./App.scss";
 function App() {
   return (
     <div className="app-container">
       <NavBar />
       <MobileNavBar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.scss";
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header_content">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <a href="/">Get Started</a>
+      </div>
+      <div className="header_image">
+        <img src="/images/illustration-working.svg" alt="ilustration" />
+      </div>
+    </header>
+  );
+}
